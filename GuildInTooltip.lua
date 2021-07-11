5ecb54b5a7f3d8da11c70131085e7dfde198eaa3
millanzarreta <millanzarreta@hotmail.com>
2021-07-11 03:33:39 +0200

    [v1.0.4 - 1.13.7] - Release
    Updated TOC file to WoW Classic 1.13.7 version

--- GuildInTooltip.lua
+++ GuildInTooltip.lua
@@ -1,8 +1,8 @@
 -- ------------------------------------------------------------ --
 -- Addon: GuildInTooltip                                        --
 --                                                              --
--- Version: 1.0.3                                               --
--- WoW Game Version: 1.13.6                                     --
+-- Version: 1.0.4                                               --
+-- WoW Game Version: 1.13.7                                     --
 -- Author: Millán - Sanguino                                    --
 --                                                              --
 -- License: GNU GENERAL PUBLIC LICENSE, Version 3, 29 June 2007 --
@@ -10,7 +10,7 @@
 
 local _G = _G
 local _
-local VERSION = "1.0.3"
+local VERSION = "1.0.4"
 
 if (not CLASSICADDGUILDTOOLTIP_HOOKED) then
 	-- Hook to add guild in tooltip when mouseover a unit frame
